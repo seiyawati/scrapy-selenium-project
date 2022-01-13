340b688617f1f87b92eed244e5b389a15cc5969c
Seiya Kawamoto <kid960805@gmail.com>
2022-01-13 14:11:23 +0900

    fix: robots.txt rules

--- scrapy_selenium/scrapy_selenium/settings.py
+++ scrapy_selenium/scrapy_selenium/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrapy_selenium.spiders'
 #USER_AGENT = 'scrapy_selenium (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
